f86a5d2b0b1f90da4d28096c9874f44878b6eb8b
Carolina Rodrigues <carolinarodrigues_100@hotmail.com>
2022-06-27 23:07:17 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,19 @@
+﻿using Agenda.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contato>().ToTable("contato");
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+        
+    }
+}
---
+++ Models/Contato.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Agenda.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        [Column("email_principal")]
+        public string Email { get; set; }
+
+        public string apelido { get; set; }
+
+        public string sexo { get; set; }
+
+       //[Column("data_nascimento")]
+      // public DateTime DataNascimento { get; set; }
+
+
+    }
+
+    
+}
